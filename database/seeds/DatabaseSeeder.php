bf6338b9b7338062173d8f54abf94cee3bda0e21
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-02-22 11:13:52 +0700

    fixing bug

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(RolesTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
         // $this->call(SuppliersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
+        // $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
