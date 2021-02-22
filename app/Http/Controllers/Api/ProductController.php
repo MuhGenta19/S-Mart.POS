e290d16022aa11753aca52c17bc2fb1835beb5bb
MuhGenta19 <muhammadgentaalam@gmail.com>
2021-02-22 11:18:34 +0700

    fixing bug

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -40,6 +40,16 @@ class ProductController extends BaseController
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
